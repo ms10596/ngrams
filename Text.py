afdc53a6289353400d8be27904f581a00c0c32ec
Mohamed Sayed <ms10596@gmail.com>
2019-03-16 21:18:22 +0200

    feat: شغال

--- Text.py
+++ Text.py
@@ -12,6 +12,7 @@ class Text:
         self.raw = raw
 
         self.words = word_tokenize(self.raw)
+        print(len(self.words))
 
         self.bigrams = list(ngrams(self.words, 2))
         self.bicounter = Counter(self.bigrams)
@@ -22,20 +23,16 @@ class Text:
     def predict(self, word):
         possibilities = []
         probabilities = []
-        for bigram,frequency in self.bicounter.items():
+        for bigram, _ in self.bicounter.items():
             word_with_other_two_words = ((word,) + bigram)
             possibilities.append(word_with_other_two_words)
             probabilities.append(
                 probability(self.tri_probability(word_with_other_two_words), self.bi_probability(bigram)))
-        possibilities, probabilities = zip(*sorted(zip(possibilities, probabilities), key=lambda t: t[1], reverse=True))
-        return [(possibilities[i],probabilities[i]) for i in range(len(probabilities[:10])) if probabilities[i] > 0]
+        possibilities, probabilities = zip(*sorted(zip(possibilities, probabilities), key=lambda t: t[1] , reverse=True))
+        return [(possibilities[i], probabilities[i]) for i in range(len(probabilities[:10])) if probabilities[i] > 0]
 
     def tri_probability(self, trigram):
-        if self.tricounter.get(trigram) is not None:
-            return self.tricounter.get(trigram) / len(trigram)
-        return 0
+        return self.tricounter.get(trigram) / len(trigram) if self.tricounter.get(trigram) is not None else 0
 
     def bi_probability(self, bigram):
-        if self.bicounter.get(bigram) is not None:
-            return self.bicounter.get(bigram) / len(bigram)
-        return 0
+        return self.bicounter.get(bigram) / len(bigram) if self.bicounter.get(bigram) is not None else 0

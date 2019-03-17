f81f6a03c5b1aa29928bffe6cf73489461490f51
mohamed magdy <mohamed14magdi@gmail.com>
2019-03-17 19:13:55 +0200

    updated

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
 from Text import Text
-from main import load
+#from main import load
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/complete", methods=['GET'])
+@app.route("/com", methods=["GET", "POST"])
 def predict():
-    txt = request.form['userInput']
-    out = load(txt)
+    f = open('corpus.txt', encoding='utf-8')
+    text = Text(f.read())
+    txt = request.form.get("userInput")
+    out = text.predict(txt)
     return render_template("index.html", out=out)
 
 if __name__ == "__main__":

a8010dd5647500bf9f5c9949cfcb86f2f82bf37b
mohamed magdy <mohamed14magdi@gmail.com>
2019-03-17 18:27:25 +0200

    main updated

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from Text import Text
-f = open('corpus.txt')
-text = Text(f.read())
+
+def load(word):
+        f = open('corpus.txt', encoding='utf-8')
+        text = Text(f.read())
+    #while True:
+        #word = input()
+        x = text.predict(word)
+    #[print(i) for i in x]
+        return x
 
 
-while True:
-    word = input()
-    x = text.predict(word)
-    [print(i) for i in x]
+
+
+
+
 

9e04a7250f95ae62484e846e523f3d5c0c31bbfd
Mohamed Sayed <ms10596@gmail.com>
2019-03-17 21:14:59 +0200

    feat: update drop down list

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request
+
 from Text import Text
-#from main import load
+
+# from main import load
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/", methods=["POST"])
 def predict():
     f = open('corpus.txt', encoding='utf-8')
     text = Text(f.read())
     txt = request.form.get("userInput")
     out = text.predict(txt)
-    return render_template("index.html", out=out)
+    print(out)
+    return render_template("index.html", out=[i[0][0] + " " + i[0][1] + " " + i[0][2] for i in out])
+
 
 if __name__ == "__main__":
     app.run()
